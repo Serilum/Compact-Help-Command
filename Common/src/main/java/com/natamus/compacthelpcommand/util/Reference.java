60bb334f50fd9db1d9ea30fa6b54ca713207655b
Rick South <rick@natamus.com>
2024-04-28 16:09:45 +0200

    Compact Help Command source code for 1.20.5.

---
+++ Common/src/main/java/com/natamus/compacthelpcommand/util/Reference.java
@@ -0,0 +1,8 @@
+package com.natamus.compacthelpcommand.util;
+
+public class Reference {
+	public static final String MOD_ID = "compacthelpcommand";
+	public static final String NAME = "Compact Help Command";
+	public static final String VERSION = "2.6";
+	public static final String ACCEPTED_VERSIONS = "[1.20.5]";
+}
