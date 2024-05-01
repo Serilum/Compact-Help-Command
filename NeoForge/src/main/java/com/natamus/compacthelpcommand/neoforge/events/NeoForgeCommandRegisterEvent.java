4714eb8ac2a14bcfcac0666b0f0236b4b62633b4
Rick South <rick@natamus.com>
2024-05-01 16:44:42 +0200

    Compact Help Command source code for 1.20.6.

---
+++ NeoForge/src/main/java/com/natamus/compacthelpcommand/neoforge/events/NeoForgeCommandRegisterEvent.java
@@ -0,0 +1,14 @@
+package com.natamus.compacthelpcommand.neoforge.events;
+
+import com.natamus.compacthelpcommand.cmds.CommandHelp;
+import net.neoforged.neoforge.event.RegisterCommandsEvent;
+import net.neoforged.bus.api.SubscribeEvent;
+import net.neoforged.fml.common.EventBusSubscriber;
+
+@EventBusSubscriber
+public class NeoForgeCommandRegisterEvent {
+	@SubscribeEvent
+	public static void registerCommands(RegisterCommandsEvent e) {
+		CommandHelp.register(e.getDispatcher());
+	}
+}
