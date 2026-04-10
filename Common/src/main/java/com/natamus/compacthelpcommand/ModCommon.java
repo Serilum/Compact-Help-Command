88e9ea478cd95093be879e09a68c42d15270142c
Rick South <rick@natamus.com>
2026-04-10 03:00:12 +0200

    Compact Help Command source code for 26.1.2.

---
+++ Common/src/main/java/com/natamus/compacthelpcommand/ModCommon.java
@@ -0,0 +1,15 @@
+package com.natamus.compacthelpcommand;
+
+import com.natamus.compacthelpcommand.config.ConfigHandler;
+
+public class ModCommon {
+
+	public static void init() {
+		ConfigHandler.initConfig();
+		load();
+	}
+
+	private static void load() {
+		
+	}
+}
