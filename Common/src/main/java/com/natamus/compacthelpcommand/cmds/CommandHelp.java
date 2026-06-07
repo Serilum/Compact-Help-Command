6ae49c045b9ba312f2edf4ae39d89ca108fda219
Rick South <38502394+ricksouth@users.noreply.github.com>
2026-06-07 19:37:41 +0200

    (v2.9) Updated the mod's text to use Collective's new translation system, which adds many languages alongside English! [+1]

--- Common/src/main/java/com/natamus/compacthelpcommand/cmds/CommandHelp.java
+++ Common/src/main/java/com/natamus/compacthelpcommand/cmds/CommandHelp.java
@@ -134,7 +134,7 @@ public class CommandHelp {
 			}
 		}
 
-		MessageFunctions.sendMessage(commandSourceStack, " Page " + page + " / " + totalpages + ", /help <page>", ChatFormatting.YELLOW);
+		MessageFunctions.sendTranslatableMessage(commandSourceStack, " ", "collective.compacthelpcommand.message.pagehelppage", ChatFormatting.YELLOW, page, totalpages);
 		return 1;
 	}
 }
